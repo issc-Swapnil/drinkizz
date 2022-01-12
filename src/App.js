132ec579e71c924ebf739cd6183fe89069c64e98
Swapnil <patilswapnil8956@gmail.com>
2022-01-12 18:52:13 +0530

    default files aded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './Layout/Header/Header';
+import Footer from './Layout/footer/footer';
+import ScrollButton from './Layout/ScrollButton/ScrollButton'
+import ScrollToTop from './Layout/ScrollToTop/ScrollToTop'
 
 function App() {
+  const Home = React.lazy(() => import('./View/Home/Home'))
+  const Compare = React.lazy(() => import('./View/Compare/Compare'))
+  const ProductDetail = React.lazy(() => import('./View/ProductDetails/ProductDetails'))
+  const Cart = React.lazy(() => import('./View/Cart/Cart'))
+  const Wishlist = React.lazy(() => import('./View/Wishlist/Wishlist'))
+  const submit = React.lazy(()=>import('./View/Cart/Submit'))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Suspense fallback={<div id="loader"></div>}>
+          <ScrollToTop />
+          <Header />
+            <Switch>
+              <Route exact path="/wishlist" component={Wishlist}></Route>
+              <Route exact path="/compare" component={Compare}></Route>
+              <Route exact path="/cart" component={Cart}></Route>
+              <Route exact path="/submit" component={submit}></Route>
+              <Route exact path="/product-details/:id" component={ProductDetail}></Route>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact component={Home}></Route>
+            </Switch>
+          <ScrollButton />
+          <Footer />
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/endpoint.js
@@ -0,0 +1,6 @@
+const BASEURL=`https://api-drinkizz.herokuapp.com`
+export const PRODUCT_URL = `${BASEURL}/public/product`
+export const CART_URL= `${BASEURL}/public/cart`
+export const WISHLIST_URL = `${BASEURL}/public/wishlist`
+export const COMPARE_URL=`${BASEURL}/public/compare`
+export const LOGIN = `${BASEURL}/user/login`
